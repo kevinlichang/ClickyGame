ea411c17716d335c9a647bbfee3628a48ecec97d
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-11-13 21:16:56 -0500

    Updates

---
+++ precache-manifest.63d6aecc1f101ebd4d91fba32adacefa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "3f1674fc50e5060be6ae",
+    "url": "/ClickyGame/static/js/main.3f1674fc.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/ClickyGame/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "3f1674fc50e5060be6ae",
+    "url": "/ClickyGame/static/css/main.d5f9841e.chunk.css"
+  },
+  {
+    "revision": "35c7c8e268fc7a5cb90bdaf7642aec5c",
+    "url": "/ClickyGame/index.html"
+  }
+];
